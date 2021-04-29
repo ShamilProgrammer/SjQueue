892a5ee697a869f5ef3e5f16e4993ebd1ec3535f
SJ <mirhome2016@gmail.com>
2021-04-29 22:15:43 +0300

    # 5 Adding new class ArrayQueue (new realization of structure "queue")

---
+++ SjQueue/Model/ArrayQueue.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+using System.Linq;
+
+namespace SjQueue.Model
+{
+    public class ArrayQueue<T>
+    {
+        private T[] items;
+        private T Head => items[Count - 1];
+        private T Tail => items[0];
+        public int Count { get; private set; }
+
+
+
+    }
+}
