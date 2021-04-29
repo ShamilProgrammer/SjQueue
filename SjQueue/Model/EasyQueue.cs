976cea88c83243b203e516a417a59ad35bbc461a
SJ <mirhome2016@gmail.com>
2021-04-29 21:18:20 +0300

    Добавьте файлы проекта.

---
+++ SjQueue/Model/EasyQueue.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SjQueue.Model
+{
+    class EasyQueue<T>
+    {
+        private List<T> items = new List<T>();
+        private T Head => items.Last();
+        private T Tail => items.First();
+        public int Count => items.Count;
+
+        public EasyQueue() { }
+
+        public EasyQueue(T data)
+        {
+            items.Add(data);
+        }
+
+
+
+    }
+}
---
+++ SjQueue/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace SjQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
