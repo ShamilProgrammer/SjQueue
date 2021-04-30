c8b67e620c501b57a79505b3ab65db540ac1ad04
SJ <mirhome2016@gmail.com>
2021-04-30 14:14:01 +0300

    # 12 Fix name bag

---
+++ SjQueue/Model/EasyQueue.cs
@@ -0,0 +1,54 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SjQueue.Model
+{
+    class EeasyQueue<T>
+    {
+        private List<T> items = new List<T>();
+        private T Head => items.Last();
+        private T Tail => items.First();
+        public int Count => items.Count;
+
+        public EeasyQueue() { }
+
+        public EeasyQueue(T data)
+        {
+            items.Add(data);
+        }
+
+        public void Enqueue(T data)
+        {
+            List<T> result = new List<T>();
+
+            result.Add(data);
+
+            foreach(T item in items)
+            {
+                result.Add(item);
+            }
+
+            items = result;
+        }
+
+        public T Dequeue()
+        {
+            if (Count != 0)
+            {
+                var item = Head;
+                items.Remove(item);
+                return item;
+            }
+            else throw new NullReferenceException("Collection is empty");
+        }
+
+        public T Peek()
+        {
+            return Head;
+        }
+
+    }
+}
